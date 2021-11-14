f4d00cd5f32058e491e2917eb6571576313b9ef3
Ueli Banholzer <ueli@whatwedo.ch>
2021-11-14 03:11:32 +0100

    chore: remove unnecessary comment

--- Formatter/DefaultFormatter.php
+++ Formatter/DefaultFormatter.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace whatwedo\CoreBundle\Formatter;
 
-/**
- * Default formatter.
- */
 class DefaultFormatter extends AbstractFormatter
 {
     public function getString($value): string
