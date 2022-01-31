0f644ac54b789d3b9791692a479df56bd7ff6fbf
tuxes3 <nicolo@whatwedo.ch>
2022-01-31 16:13:35 -0500

    feat select working

--- Resources/assets/controllers/select_controller.js
+++ Resources/assets/controllers/select_controller.js
@@ -2,17 +2,36 @@ import { Controller } from '@hotwired/stimulus';
 import TomSelect from 'tom-select';
 import 'tom-select/dist/css/tom-select.css';
 
+
 export default class extends Controller {
+    static values = {
+        url: String
+    }
     connect() {
-        new TomSelect(this.element,{
+        const urlValue = this.urlValue;
+        const settings = {
             plugins: [],
             persist: false,
-            createOnBlur: true,
-            create: true,
-            sortField: {
+        };
+        if (urlValue !== '') {
+            settings.sortField = {
                 field: "text",
                 direction: "asc"
+            };
+            settings.valueField = 'id';
+            settings.searchField = 'label';
+            settings.labelField = 'label';
+            settings.load = function (query, callback) {
+                const url = urlValue + '?q=' + encodeURIComponent(query);
+                fetch(url)
+                    .then(response => response.json())
+                    .then(json => {
+                        callback(json.items);
+                    }).catch(()=>{
+                    callback();
+                });
             }
-        });
+        }
+        new TomSelect(this.element, settings);
     }
 }
