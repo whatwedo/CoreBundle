f8d9c7ab866743968f5137d3488b2d49e9a1e326
Maurizio Monticelli <rugbymauri@gmail.com>
2022-02-15 16:25:00 +0100

    feat(formatter): enumformatter check null

--- src/Formatter/EnumFormatter.php
+++ src/Formatter/EnumFormatter.php
@@ -15,6 +15,9 @@ class EnumFormatter extends AbstractFormatter
 
     public function getString($enum): string
     {
+        if (!$enum) {
+            return '';
+        }
         return $this->translator->trans($enum->value);
     }
 }
