ac485f42fa8713e696c71a1a6bafda5c07cdb740
tuxes3 <nicolo@whatwedo.ch>
2022-03-03 11:28:11 -0500

    Feature/1.0 dev action (#9)
    unified actions into core bundle

---
+++ src/Action/Action.php
@@ -0,0 +1,110 @@
+<?php
+
+declare(strict_types=1);
+
+namespace whatwedo\CoreBundle\Action;
+
+use Symfony\Component\Form\Util\StringUtil;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class Action
+{
+    protected array $defaultOptions = [
+        'label' => null,
+        'attr' => [],
+        'icon' => null,
+        'route' => null,
+        'route_parameters' => [],
+        'voter_attribute' => null,
+        'priority' => 0,
+        'confirmation' => null,
+    ];
+
+    /**
+     * it's possible to pass functions as option value to create dynamic labels, routes and more.
+     * TODO: create docs.
+     */
+    public function __construct(
+        protected string $acronym,
+        protected array $options
+    ) {
+        $resolver = new OptionsResolver();
+        $resolver->setDefaults(array_merge([
+            'block_prefix' => StringUtil::fqcnToBlockPrefix(static::class),
+        ], $this->defaultOptions));
+        $this->options = $resolver->resolve($this->options);
+    }
+
+    public function getOption(string $name)
+    {
+        if (! $this->hasOption($name)) {
+            throw new \InvalidArgumentException(sprintf('Option "%s" for %s does not exist.', $name, static::class));
+        }
+        return $this->options[$name];
+    }
+
+    public function setOption($name, $value): static
+    {
+        if (! $this->hasOption($name)) {
+            throw new \InvalidArgumentException(sprintf('Option "%s" for %s does not exist.', $name, static::class));
+        }
+
+        $this->options[$name] = $value;
+
+        return $this;
+    }
+
+    public function hasConfirmation(): bool
+    {
+        return $this->getOption('confirmation') !== null;
+    }
+
+    public function getConfirmation(string $key): string
+    {
+        $confirmation = $this->getOption('confirmation');
+        return $confirmation[$key] ?? '';
+    }
+
+    public function hasOption(string $name): bool
+    {
+        return isset($this->options[$name]) || array_key_exists($name, $this->options);
+    }
+
+    public function getAcronym(): string
+    {
+        return $this->acronym;
+    }
+
+    public function getRoute(): ?string
+    {
+        return $this->getOption('route');
+    }
+
+    public function getRouteParameters($entity = null)
+    {
+        if (is_callable($this->getOption('route_parameters'))) {
+            return $this->getOption('route_parameters')($entity);
+        }
+
+        return $this->getOption('route_parameters');
+    }
+
+    public function getIcon(): ?string
+    {
+        return $this->getOption('icon');
+    }
+
+    public function getLabel(): ?string
+    {
+        return $this->getOption('label');
+    }
+
+    public function getVoterAttribute(): ?string
+    {
+        return $this->getOption('voter_attribute');
+    }
+
+
+
+
+}
