728b49311424cef05c10a91fab8c3868f49d4415
Sandro Rüfenacht <sandro@whatwedo.ch>
2022-11-29 10:17:49 +0100

    feat(dropdown): only allow one to be open

--- src/Resources/assets/controllers/dropdown_controller.js
+++ src/Resources/assets/controllers/dropdown_controller.js
@@ -1,12 +1,30 @@
-import Dropdown from "stimulus-dropdown"
+import {Controller} from "@hotwired/stimulus";
 
-export default class extends Dropdown {
+export default class extends Controller {
     connect() {
         super.connect()
     }
 
     toggle (event) {
-        super.toggle()
+        let dropdownButton = this.element.querySelector("button");
+        let dropdownDiv = this.element.querySelector('[data-whatwedo--core-bundle--dropdown-target="menu"]');
+
+        if (dropdownDiv && dropdownDiv.classList.contains('hidden')) {
+            // Hide all other dropdowns
+            document
+                .querySelectorAll('.whatwedo_table-actions[data-action^="whatwedo--core-bundle--dropdown#toggle"]')
+                .forEach((element) => {
+                    let eventDropdownDiv = element.parentNode.querySelector('[data-whatwedo--core-bundle--dropdown-target="menu"]');
+
+                    if (element !== dropdownButton && eventDropdownDiv && !eventDropdownDiv.classList.contains("hidden"))
+                        eventDropdownDiv.classList.add("hidden");
+                })
+
+            // Show currently focussed dropdown
+            dropdownDiv.classList.remove('hidden');
+        } else {
+            dropdownDiv.classList.add('hidden');
+        }
     }
 
     close (event) {
