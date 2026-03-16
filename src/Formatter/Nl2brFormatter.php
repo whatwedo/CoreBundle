4bb82a3a6be190ede3db1041cd985069264112a6
Arno Egli <arno@whatwedo.ch>
2026-03-16 11:28:45 +0100

    fix: handle null values in Nl2brFormatter
    PHP 8 is strict about passing null to nl2br(). Cast to string to handle
    null values gracefully.
    
    Signed-off-by: Ueli Banholzer <ueli@whatwedo.ch>
    Signed-off-by: Arno Egli <arno@whatwedo.ch>

--- src/Formatter/Nl2brFormatter.php
+++ src/Formatter/Nl2brFormatter.php
@@ -8,11 +8,11 @@ class Nl2brFormatter extends AbstractFormatter
 {
     public function getString(mixed $value): string
     {
-        return $value;
+        return (string) $value;
     }
 
     public function getHtml(mixed $value): string
     {
-        return nl2br($value);
+        return nl2br((string) $value);
     }
 }
