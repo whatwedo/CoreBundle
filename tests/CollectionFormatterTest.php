abfbc7be9ef3a0bb6459acbeaa964fd5d7f0827c
Sandro Rüfenacht <sandro@whatwedo.ch>
2026-08-12 09:46:48 +0200

    test(CollectionFormatter): add tests for getString() and object casting

--- tests/CollectionFormatterTest.php
+++ tests/CollectionFormatterTest.php
@@ -8,9 +8,41 @@ use whatwedo\CoreBundle\Formatter\CollectionFormatter;
 
 class CollectionFormatterTest extends AbstractFormatterTest
 {
-    public function testFormatter(): void
+    public function testGetHtml(): void
     {
         $formatter = $this->getFormatter(CollectionFormatter::class);
-        self::assertSame('<ul><li>eins</li><li>zwei</li></ul>', $formatter->getHtml(['eins', 'zwei']));
+        self::assertSame('<ul><li>one</li><li>two</li></ul>', $formatter->getHtml(['one', 'two']));
+    }
+
+    public function testGetHtmlWithObjects(): void
+    {
+        $object = new class() {
+            public function __toString(): string
+            {
+                return 'entity-value';
+            }
+        };
+
+        $formatter = $this->getFormatter(CollectionFormatter::class);
+        self::assertSame('<ul><li>entity-value</li></ul>', $formatter->getHtml([$object]));
+    }
+
+    public function testGetString(): void
+    {
+        $formatter = $this->getFormatter(CollectionFormatter::class);
+        self::assertSame('one, two', $formatter->getString(['one', 'two']));
+    }
+
+    public function testGetStringWithObjects(): void
+    {
+        $object = new class() {
+            public function __toString(): string
+            {
+                return 'entity-value';
+            }
+        };
+
+        $formatter = $this->getFormatter(CollectionFormatter::class);
+        self::assertSame('entity-value', $formatter->getString([$object]));
     }
 }
